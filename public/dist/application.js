2b22ae036378486809e0405c87a3eedfad1b10b6
Ryan Huffaker <ryanjayhuffaker@gmail.com>
2015-06-23 18:09:27 -0600

    modified demo text(2)

--- public/dist/application.js
+++ public/dist/application.js
@@ -1397,53 +1397,60 @@ angular.module('core').factory('demoDeck', ['StackUtils', 'PanelUtils', function
                 panelType: 'deckDemo',
                 x_coord: 0,
                 y_coord: 0,
-                content: 'Here is a basic demonstration of a card-deck layout. While additional features may be accessed via the menu bar, this simple demo serves to show how card-objects may be manipulated by the user in much the same way as a physical deck of cards.'
+                content: 'Here is a basic demonstration of a card-deck layout. While additional features may be accessed via the menu bar, this simple demo serves to show how card-objects may be manipulated by the user, much same as you might a physical deck of cards.'
             },
             {
                 _id: 'demoCard2',
                 panelType: 'deckDemo',
                 x_coord: 15,
                 y_coord: 0,
-                aboveId: 'demoCard3',
-                content: 'Note that the content of each card may vary, and has no relation whatsoever to its actual position. Click again to cover it back up.'
+                content: 'For starters, cards can be moved around freely, automatically returning to their position unless moved to a new position.'
             },
             {
                 _id: 'demoCard3',
                 panelType: 'deckDemo',
-                x_coord: 15,
-                y_coord: 3,
-                belowId: 'demoCard2',
-                content: 'Cards may be stacked vertically. Click on an covered card to uncover it.'
+                x_coord: 30,
+                y_coord: 0,
+                aboveId: 'demoCard4',
+                content: 'Note that the content of each card may vary, and has no relation whatsoever to its actual position. Click again to cover it back up.'
             },
             {
                 _id: 'demoCard4',
                 panelType: 'deckDemo',
                 x_coord: 30,
-                y_coord: 0,
-                rightId: 'demoCard5',
-                content: 'Cards can also be stacked horizontally, but only if they are not already stacked vertically.'
+                y_coord: 3,
+                belowId: 'demoCard3',
+                content: 'Cards may be stacked vertically. Click on an covered card to uncover it.'
             },
             {
                 _id: 'demoCard5',
                 panelType: 'deckDemo',
-                x_coord: 33,
+                x_coord: 45,
                 y_coord: 0,
-                leftId: 'demoCard4',
-                content: 'Both vertical and horizontal stacks may be reordered as a single entity.'
+                rightId: 'demoCard6',
+                content: 'Cards can also be stacked horizontally, but only if they are not already stacked vertically.'
             },
             {
                 _id: 'demoCard6',
                 panelType: 'deckDemo',
                 x_coord: 48,
                 y_coord: 0,
-                content: 'The content of each card can also be modified by the user (TODO: add input field).'
+                leftId: 'demoCard5',
+                content: 'Both vertical and horizontal stacks may be reordered as a single entity.'
             },
             {
                 _id: 'demoCard7',
                 panelType: 'deckDemo',
                 x_coord: 63,
                 y_coord: 0,
-                content: 'More coming soon! Recently refactored panel "getter" and "setter" methods to return a specific panel based upon its unique objectId instead of its x/y coordinates. This should allow for more reliable and flexible stacking, including the possibility of overlapping horizontal and vertical stacks.'
+                content: 'The content of each card can also be modified by the user (TODO: add input field).'
+            },
+            {
+                _id: 'demoCard8',
+                panelType: 'deckDemo',
+                x_coord: 78,
+                y_coord: 0,
+                content: 'More coming soon! Recently refactored panel "getter" and "setter" methods to return a specific panel based upon its unique objectId, instead of its x/y coordinates. This should allow for more reliable and flexible stacking, including the possibility of overlapping horizontal and vertical stacks.'
             }
         ]
     };
